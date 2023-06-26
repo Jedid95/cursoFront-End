d0d47b429873924740ef02c4a146b96e768a2780
Jedid95 <jedid.santos@gmail.com>
2023-06-26 20:30:44 +0100

    aula 06 condicoes

---
+++ JavaScript/aula06-condicoes/functions.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    // == Igual
+    // === Identico
+    // != Diferente
+    // !==
+    // >=
+    // <=
+    // >
+    // <
+
+    var nome = "Jedid";
+
+    if(nome == "Jedid"){
+        alert("Condição verdadeira");
+    } 
+    else if(nome="João"){
+        alert("Condição para João verdadeira");
+    }
+    else{
+        alert("Condição falsa");
+    }
+
+
+
+}
