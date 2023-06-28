64ea73d3fd349734aa07d825401bac4d3bf27963
Jedid95 <jedid.santos@gmail.com>
2023-06-29 00:25:58 +0100

    funções para iniciar com jquery

---
+++ JQuery/js/functions.js
@@ -0,0 +1,15 @@
+/*$(function(){
+    alert("ola mundo");
+})*/
+
+/*$(document).ready(function(){
+    console.log("ola mundo");
+})*/
+
+/*$(window).on('load', function(){
+    alert("ola mundo");
+})*/
+
+$(function(){
+    
+})
