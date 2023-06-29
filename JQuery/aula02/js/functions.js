25ed9826bc84beeae1efb336d05fa0b497f3264d
Jedid95 <jedid.santos@gmail.com>
2023-06-29 22:19:29 +0100

    aula 02 - manipulando css com jquery

--- JQuery/aula02/js/functions.js
+++ JQuery/aula02/js/functions.js
@@ -1,3 +1,10 @@
 $(function(){
-    
-})
+    $('#teste').css('background-color','#ccc');
+    $('#teste').css('padding-left', '50px');
+
+    console.log($('#teste').css('padding-left'));
+    setTimeout(function(){
+        $('#teste').css('background-color','green');
+        $('#teste p').css('color','red');
+    },2000);
+});
