52d173074d2f8a144cdf66a000176dbed78c0293
Jedid95 <jedid.santos@gmail.com>
2023-07-04 23:23:57 +0100

    AULA 08

--- JQuery/aula08-eventos_dinamicos/js/functions.js
+++ JQuery/aula08-eventos_dinamicos/js/functions.js
@@ -1,5 +1,5 @@
 $(function(){
-
+    /*
     $('a').click(function(){
         $('.teste').css('display','block');
         return false;
@@ -7,8 +7,19 @@ $(function(){
 
     $('body').on('click','a',function(){
         alert('Olá Mundo!');
+        return false;
     })
 
     $('body').html('<a href="">Meu Link!</a>');
+
+    */
+
+    var func = function(){
+        $(this).css('background','green');
+        console.log($(this).index());
+    }
+
+    /*$('input[name=nome_pessoa]').keyup(func);*/
+    $('input[name=nome_pessoa]').keydown(func);
 
 });
