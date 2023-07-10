6408c7088ccb1fd52b89a1162dd3ebfdf20ce969
Jedid95 <jedid.santos@gmail.com>
2023-07-10 23:19:03 +0100

    expressões regulares

--- JQuery/criando_janela_modal_formulario/js/functions.js
+++ JQuery/criando_janela_modal_formulario/js/functions.js
@@ -1,5 +1,9 @@
 $(function(){
 
+    /*var frase = 'ola mundo';
+    var verifica = frase.match(/ola/);
+    console.log(verifica);*/
+
     function abrirJanela(){
 
         $('.btn').click(function(e){
@@ -21,8 +25,44 @@ $(function(){
         })
     }
 
+    $('form#form1').submit(function(){
+        var nome = $('input[name=nome]').val();
+        var email = $('input[name=email]').val();
+        var telefone = $('input[name=telefone]').val();
+        
+        //verifica nome
+        var amount = nome.split(' ').length;
+        var splitStr = nome.split(' ');
+        if(amount >= 2){
+            //console.log('Temos pelo menos um sobrenome')
+            for(var i = 0; i < amount; i++){
+                console.log(splitStr[i]);
+                if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
+                    console.log('Nossa condição de nome bateu');
+                }else{
+                    aplicarCampoInvalido($('input[name=nome]'));
+                    return false;
+                }
+            }
+        } else{
+            aplicarCampoInvalido($('input[name=nome]'));
+            return false; 
+        }
+        return false;
+        //Se chegou até o final é pq está tudo ok
+    })
+
+
+
+    function aplicarCampoInvalido(el){
+        el.css('border', '2px solid red');
+        el.data('invalido','true');
+        el.val('Campo Inválido');
+    }
+
+
     abrirJanela();
-    verificarCliqueFechar()
+    verificarCliqueFechar();
 
     
 
